cdab994b825068c5e1e098fd5a702032a5a6d266
Jason Zhang <jiahzhang23@m.fudan.edu.cn>
2025-09-15 09:20:47 +0000

    -

--- examples/exp/smolvla-bs32-n8-mb256-lr5e6-kl004-rm7b-trainset.sh
+++ examples/exp/smolvla-bs32-n8-mb256-lr5e6-kl004-rm7b-trainset.sh
@@ -21,7 +21,7 @@ NUM_GPUS=8
 NUM_NODES=1 
 ALIGN_PATH="/inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl/align.json"
 
-HYDRA_FULL_ERROR=1 python -m verl.trainer.main_ppo \
+HYDRA_FULL_ERROR=1 python -m verl_vla.trainer.main_ppo \
     data.task_suite_name=$DATASET_NAME \
     data.libero_raw_data_dir=$DATASET_PATH \
     data.num_trials_per_task=50 \
