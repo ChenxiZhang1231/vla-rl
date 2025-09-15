199be4d842c4869261f336cb2b8002f97cdfb032
Jason Zhang <jiahzhang23@m.fudan.edu.cn>
2025-09-15 09:12:37 +0000

    -

--- qwenvl_chat/shell/qwen25vl_7b-serve.sh
+++ qwenvl_chat/shell/qwen25vl_7b-serve.sh
@@ -1,4 +1,4 @@
-vllm serve work_dirs/qwen2.5-vl-7b-sft-lora-baseline/qwen2.5-vl-7b-sft-lora-baseline-merge-3ep \
+vllm serve /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline/qwen2.5-vl-7b-sft-lora-baseline-merge-3ep \
     --port 18901 \
     --gpu-memory-utilization 0.3 \
     --max-model-len 32768 \
