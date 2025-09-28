d66243dd26acd3ad6bcdb95c44ebfda2d32b9602
Jason Zhang <jiahzhang23@m.fudan.edu.cn>
2025-09-28 15:43:48 +0000

    -

--- qwenvl_chat/shell/batch-eval-rm.sh
+++ qwenvl_chat/shell/batch-eval-rm.sh
@@ -1,6 +1,6 @@
 
 
-OUTPUT_DIR="work_dirs/sft-7b-lora128-5k"
+OUTPUT_DIR="work_dirs/sft-7b-lora128-5k-ref"
 
 TAG="855steps_vote5_pass3"
-python utils/eval_reward_model.py --output_dir $OUTPUT_DIR --tag $TAG
+python utils/eval_reward_model_ref.py --output_dir $OUTPUT_DIR --tag $TAG
--- qwenvl_chat/shell/merge_lora.sh
+++ qwenvl_chat/shell/merge_lora.sh
@@ -6,34 +6,34 @@ MODEL_NAME="/inspire/ssd/project/robotsimulation/public/huggingface_models/Qwen2
 export PYTHONPATH=src:$PYTHONPATH
 
 python src/merge_lora_weights.py \
-    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/checkpoint-171 \
+    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/checkpoint-171 \
     --model-base $MODEL_NAME  \
-    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-171step \
+    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-171step \
     --safe-serialization
 
 
 python src/merge_lora_weights.py \
-    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/checkpoint-342 \
+    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/checkpoint-342 \
     --model-base $MODEL_NAME  \
-    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-342step \
+    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-342step \
     --safe-serialization
 
 
 python src/merge_lora_weights.py \
-    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/checkpoint-513 \
+    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/checkpoint-513 \
     --model-base $MODEL_NAME  \
-    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-513step \
+    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-513step \
     --safe-serialization
 
 
 python src/merge_lora_weights.py \
-    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/checkpoint-684 \
+    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/checkpoint-684 \
     --model-base $MODEL_NAME  \
-    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-684step \
+    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-684step \
     --safe-serialization
 
 python src/merge_lora_weights.py \
-    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/checkpoint-855 \
+    --model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/checkpoint-855 \
     --model-base $MODEL_NAME  \
-    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-855step \
+    --save-model-path /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-ref/qwen2.5-vl-7b-sft-lora-baseline-5k-r128-merge-855step \
     --safe-serialization
