9dc47b69c6252e20eca3600720b6a92dffa6bf7d
Jason Zhang <jiahzhang23@m.fudan.edu.cn>
2025-10-04 16:24:51 +0000

    -

--- qwenvl_chat/shell/multi_nodes_train.sh
+++ qwenvl_chat/shell/multi_nodes_train.sh
@@ -1,13 +1,16 @@
 
 nvidia-smi 
-cd /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/shell
+source /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/miniconda3/etc/profile.d/conda.sh
+conda activate lrm_wm2
+
+cd /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat
 
 export NNODES=${PET_NNODES}
 export NRANK=${PET_NODE_RANK}
 export MASTER_ADDR=${MASTER_ADDR}
 export MASTER_PORT=${MASTER_PORT}
 
-export GPUS=2  # for debug, 2gpus per nodes
-export BATCH_SIZE=32. # for torchrun, total batchsize = BATCH_SIZE * NNODES
+export GPUS=8
+export BATCH_SIZE=32 # for torchrun, total batchsize = BATCH_SIZE * NNODES
 export PER_DEVICE_BATCH_SIZE=1
-bash train_debug.sh
+bash shell/train_debug.sh
