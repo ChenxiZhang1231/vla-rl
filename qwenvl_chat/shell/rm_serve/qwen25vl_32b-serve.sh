a8c96b237ee8aefea715b7cf9c7388fa9c6a5597
Jason Zhang <jiahzhang23@m.fudan.edu.cn>
2025-10-23 15:56:43 +0000

    -

--- qwenvl_chat/shell/rm_serve/qwen25vl_32b-serve.sh
+++ qwenvl_chat/shell/rm_serve/qwen25vl_32b-serve.sh
@@ -1,4 +1,5 @@
-vllm serve work_dirs/qwen2.5-vl-32b-sft-lora-baseline/qwen2.5-vl-32b-sft-lora-baseline-merge-60step \
+# vllm serve work_dirs/qwen2.5-vl-32b-sft-lora-baseline/qwen2.5-vl-32b-sft-lora-baseline-merge-60step \
+vllm serve /inspire/ssd/project/robotsimulation/public/huggingface_models/Qwen2.5-VL-32B-Instruct \
     --port 18901 \
     --gpu-memory-utilization 0.3 \
     --max-model-len 32768 \
---
+++ qwenvl_chat/shell/rm_serve/qwen25vl_7b_full-bridge-serve-8gpu.sh
@@ -0,0 +1,13 @@
+# internvl
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# vllm serve /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-full-baseline-5k-64tokens-10ep/checkpoint-1710 \
+vllm serve /inspire/ssd/project/robotsimulation/public/users/zhangjiahui/vla-rl-dev/qwenvl_chat/work_dirs/qwen2.5-vl-7b-sft-full-bridge-4tasks/checkpoint-222 \
+  --served-model-name "judge" \
+  --port 18901 \
+  --gpu-memory-utilization 0.25 \
+  --max-model-len 8192 \
+  --tensor-parallel-size 1 \
+  --pipeline-parallel-size 1 \
+  --data-parallel-size 8 \
+  --distributed-executor-backend mp \
+  --disable-log-requests
